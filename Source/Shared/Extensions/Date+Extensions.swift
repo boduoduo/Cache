ad6f74930f2db0546256c8948f1873150a4333a9
Maxb <maxiaobo02@corp.netease.com>
2022-08-08 15:38:55 +0800

    format codes

--- Source/Shared/Extensions/Date+Extensions.swift
+++ Source/Shared/Extensions/Date+Extensions.swift
@@ -4,9 +4,9 @@ import Foundation
  Helper NSDate extension.
  */
 extension Date {
-
-  /// Checks if the date is in the past.
-  var inThePast: Bool {
-    return timeIntervalSinceNow < 0
-  }
+    
+    /// Checks if the date is in the past.
+    var inThePast: Bool {
+        return timeIntervalSinceNow < 0
+    }
 }
